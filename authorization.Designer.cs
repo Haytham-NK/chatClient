2f79b2dc09b0943f3aebd0dee31706db592b9084
Haytham-NK <64586817+Haytham-NK@users.noreply.github.com>
2021-05-29 02:51:44 +0300

    Initial commit

---
+++ authorization.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace chatClient
+{
+    partial class authorization
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_enter = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.tb_password = new System.Windows.Forms.TextBox();
+            this.tb_login = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_enter
+            // 
+            this.btn_enter.Font = new System.Drawing.Font("Tahoma", 11.25F);
+            this.btn_enter.Location = new System.Drawing.Point(12, 120);
+            this.btn_enter.Name = "btn_enter";
+            this.btn_enter.Size = new System.Drawing.Size(100, 40);
+            this.btn_enter.TabIndex = 0;
+            this.btn_enter.Text = "Вход";
+            this.btn_enter.UseVisualStyleBackColor = true;
+            this.btn_enter.Click += new System.EventHandler(this.btn_enter_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_exit.Location = new System.Drawing.Point(122, 120);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(100, 40);
+            this.btn_exit.TabIndex = 1;
+            this.btn_exit.Text = "Выход";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // tb_password
+            // 
+            this.tb_password.Location = new System.Drawing.Point(12, 84);
+            this.tb_password.Multiline = true;
+            this.tb_password.Name = "tb_password";
+            this.tb_password.Size = new System.Drawing.Size(210, 30);
+            this.tb_password.TabIndex = 2;
+            // 
+            // tb_login
+            // 
+            this.tb_login.Location = new System.Drawing.Point(12, 30);
+            this.tb_login.Multiline = true;
+            this.tb_login.Name = "tb_login";
+            this.tb_login.Size = new System.Drawing.Size(210, 30);
+            this.tb_login.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 11.25F);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Логин";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 11.25F);
+            this.label2.Location = new System.Drawing.Point(9, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Пароль";
+            // 
+            // authorization
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(243, 174);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_login);
+            this.Controls.Add(this.tb_password);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_enter);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "authorization";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Авторизация";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_enter;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.TextBox tb_password;
+        private System.Windows.Forms.TextBox tb_login;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ authorization.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chatClient
+{
+    public partial class authorization : Form
+    {
+        public string Log { get; set; }
+        public authorization()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_enter_Click(object sender, EventArgs e)
+        {
+
+
+            if(tb_login.Text != "" && tb_password.Text != "")
+            {
+                try
+                {
+                   
+                    Form ffm = new chatForm();
+                    ffm.Owner = this;
+                    ffm.ShowDialog();
+                    ffm.Show();
+                    Log = tb_login.Text;
+                    this.Hide();
+                }
+                catch
+                {
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Необходимо заполнить все поля!", "Закрыть", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+
+            }
+        }
+    }
+}
